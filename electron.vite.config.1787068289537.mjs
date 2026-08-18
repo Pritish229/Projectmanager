64e25f1d81ec8582a1cceb4c6c25b104ade13fb6
Pritish229 <pritishdash29@gmail.com>
2026-08-18 21:21:34 +0530

    feat: initialize electron-vite configuration with main, preload, and renderer build settings

---
+++ electron.vite.config.1787068289537.mjs
@@ -0,0 +1,47 @@
+// electron.vite.config.ts
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+var __electron_vite_injected_dirname = "H:\\Projectmanager\\Project Workspace Manager";
+var electron_vite_config_default = defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__electron_vite_injected_dirname, "src/main/index.ts")
+        }
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__electron_vite_injected_dirname, "src/preload/index.ts")
+        }
+      }
+    }
+  },
+  renderer: {
+    root: resolve(__electron_vite_injected_dirname, "src/renderer"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(__electron_vite_injected_dirname, "src/renderer")
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__electron_vite_injected_dirname, "src/renderer/index.html")
+        }
+      }
+    }
+  }
+});
+export {
+  electron_vite_config_default as default
+};
